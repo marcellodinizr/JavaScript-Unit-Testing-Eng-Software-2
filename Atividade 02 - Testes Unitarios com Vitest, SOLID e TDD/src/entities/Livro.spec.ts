da8467c5837355ee6becc1f5b3c4f34fbf89449a
marcellodinizr <marcellodinizrocha@gmail.com>
2022-10-19 14:54:58 -0300

    test: deve realizar emprestimo de um livro

--- Atividade 02 - Testes Unitarios com Vitest, SOLID e TDD/src/entities/Livro.spec.ts
+++ Atividade 02 - Testes Unitarios com Vitest, SOLID e TDD/src/entities/Livro.spec.ts
@@ -3,7 +3,19 @@ import { describe, expect, it } from 'vitest';
 import { Livro } from './Livro';
 
 describe('Classe: Livro', () => {
-	it('criar um livro', () => {
+	it('deve realizar emprestimo de um livro não reservado', () => {
+		const livro = new Livro({
+		 autor: 'John Doe',
+		 titulo: 'Game of Thrones',
+		 emprestado: true,
+		 reservado: false,
+		 historico: [],
+		})
+	 
+		expect(livro.reservado).toBe(false);
+	})
+
+	it('deve tentar realizar emprestimo de um livro reservado', () => {
 		const livro = new Livro({
 		 autor: 'John Doe',
 		 titulo: 'Game of Thrones',
@@ -12,7 +24,7 @@ describe('Classe: Livro', () => {
 		 historico: [],
 		})
 	 
-		expect(livro).toBeInstanceOf(Livro);
-})
+		expect(livro.reservado).not.toBe(false);
+	})
 
 });
